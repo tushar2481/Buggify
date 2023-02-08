28fe03c430272d7181d4a964546d554d94b0d812
Mohit <mohit.patel1966@gmail.com>
2023-02-08 13:45:02 +0530

    footer

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,30 +1,26 @@
-import Homes1 from './Homes1';
-import Homes2 from './Homes2';
-import Homes3 from './Homes3';
+import Homes1 from "./Homes1";
+import Homes2 from "./Homes2";
+import Homes3 from "./Homes3";
 
-import Homes6 from './Homes6';
+import Homes6 from "./Homes6";
 
-import Homes7 from './Homes7';
-import Homes8 from './Homes8';
-import Homes9 from './Homes9';
+import Homes7 from "./Homes7";
+import Homes8 from "./Homes8";
+import Homes9 from "./Homes9";
 
 function Home() {
-    return (
-        <>
-            <Homes1 />
-            <Homes2 />
-            <Homes3 />
+	return (
+		<>
+			<Homes1 />
+			<Homes2 />
+			<Homes3 />
+			<Homes6 />
 
-            <Homes6 />
-
-
-
-
-            <Homes7 />
-            <Homes8 />
-            <Homes9 />
-        </>
-    )
+			<Homes7 />
+			<Homes8 />
+			<Homes9 />
+		</>
+	);
 }
 
 export default Home;
--- src/component/Program.jsx
+++ src/component/Program.jsx
@@ -1,10 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import './Program.css';
 
-const Program = () => {
-    return (
-        <>
-        </>
-    )
-}
+const Footer = () => {
+  const [showAbout, setShowAbout] = useState(false);
 
-export default Program; 
+  return (
+    <footer style={{ backgroundColor: "#141414", display: "flex" }}>
+      <img
+        src="logo.png"
+        alt="Buggify logo"
+        style={{ marginLeft: "10rem" }}
+      />
+      <ul style={{ marginLeft: "15rem", listStyleType: "none" }}>
+        <li>
+          <h3>Business</h3>
+          <ul style={{ display: showAbout ? "block" : "none" }}>
+            <li>Services</li>
+            <li>Pricing</li>
+            <li>Case Studies</li>
+          </ul>
+        </li>
+        <li>
+          <h3>Researcher</h3>
+          <ul style={{ display: showAbout ? "block" : "none" }}>
+            <li>Methods</li>
+            <li>Findings</li>
+            <li>Publications</li>
+          </ul>
+        </li>
+        <li>
+          <h3>Academy</h3>
+          <ul style={{display: showAbout ? "block" : "none" }}>
+            <li>Courses</li>
+            <li>Instructors</li>
+            <li>Enrollment</li>
+          </ul>
+        </li>
+        <li>
+          <h3>About</h3>
+          <ul style={{ display: showAbout ? "block" : "none" }}>
+            <li>Company</li>
+            <li>Careers</li>
+            <li>Contact</li>
+          </ul>
+          <button onClick={() => setShowAbout(!showAbout)}>
+            {showAbout ? "Close" : "Open"}
+          </button>
+        </li>
+      </ul>
+    </footer>
+  );
+};
+
+export default Footer;
