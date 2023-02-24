392861017dd8e35ac73200e74afc8dadb798ad59
tushar2481 <tusharmotka.19.cs@iite.indusuni.ac.in>
2023-02-24 23:10:51 +0530

    APIdev

---
+++ src/models/userConn.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+// defining Schema for Business
+const businessSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        unique: true,
+        required: true,
+        minlength: 5
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 7
+    },
+    password:{
+        type: String,
+        required: true,
+        minlength: 8
+    },
+    company_name:{
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength:30
+    },
+    position:{
+        type: String,
+        required: true,
+        minlength: 5
+    },
+    country:{
+        type: String,
+        required: true
+    }
+})
+
+// defining model for new collection
+const businessUsers = new mongoose.model('businessUser', businessSchema);
+
+module.exports = businessUsers;
