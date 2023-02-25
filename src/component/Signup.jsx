ef1a65e67e6e4895dc1be8b58d1624d21283033e
tushar2481 <tusharmotka.19.cs@iite.indusuni.ac.in>
2023-02-25 21:52:21 +0530

    APIs are deprecated

--- src/component/Signup.jsx
+++ src/component/Signup.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import './signin.css';
-import { Form, Input, Label, Dropdown, Checkbox, Button } from "semantic-ui-react";
+import './Login.css';
+// import { Form, Input, Label, Dropdown, Checkbox, Button } from "semantic-ui-react";
 
 const SignInForm = () => {
-    const [username, setUsername] = useState("");
+    const [username, setUserName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
@@ -12,112 +13,91 @@ const SignInForm = () => {
     const [country, setCountry] = useState("");
     const [acceptedTerms, setAcceptedTerms] = useState(false);
 
-
-    const countries = [
-        { key: "af", value: "af", text: "Afghanistan" },
-        { key: "ax", value: "ax", text: "Aland Islands" },
-        { key: "al", value: "al", text: "Albania" },
-        // ... more countries here
-    ];
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        // add code to handle form submission here
+    var data = {
+        username: username,
+        email: email,
+        password: password,
+        confirmPassword: confirmPassword,
+        companyName: companyName,
+        position: position,
+        country: country,
+        acceptedTerms: acceptedTerms
+    }
+    const jsonData = JSON.stringify(data);
+    // const countries = [
+    //     { key: "af", value: "af", text: "Afghanistan" },
+    //     { key: "ax", value: "ax", text: "Aland Islands" },
+    //     { key: "al", value: "al", text: "Albania" },
+    //     // ... more countries here
+    // ];
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if(password==confirmPassword){
+            try{
+                alert(typeof(jsonData));
+                const response = await fetch(`http://127.0.0.1:8000/business`, {
+                    method: 'POST',
+                    mode: 'no-cors',
+                    header: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: jsonData
+                     });
+                console.log(response.json());
+            } catch (error) {
+                    console.log(error);
+                  }
+        };
     };
 
+    const handleAcceptedTerms = () => {
+        setAcceptedTerms(!acceptedTerms);
+    };
     return (
         <div className="signin">
 
-            <Form onSubmit={handleSubmit}>
+            <form className="login-form">
                 <h2 style={{ textAlign: "center" }} className="signin-h">Sign In</h2>
-                <Form.Field>
+                <div>
                     <label className="signin-label">User Name*</label>
-                    <input
-                        className="signin-input"
-                        type="text"
-                        name="userName"
-                        value={username}
-                        onChange={(event) => setUserName(event.target.value)}
-                    />
-                </Form.Field>
-                <Form.Field>
+                    <input className="signin-input" type="text" name="userName" value={username} onChange={(event) => setUserName(event.target.value)} required/>
+                </div>
+                <div>
                     <label className="signin-label">Email*</label>
-                    <input
-                        className="signin-input"
-                        type="text"
-                        name="email"
-                        value={email}
-                        onChange={(event) => setEmail(event.target.value)}
-                    />
-                </Form.Field>
+                    <input className="signin-input" type="text" name="email" value={email} onChange={(event) => setEmail(event.target.value)} required/>
+                </div>
 
-                <Form.Field>
+                <div>
                     <label className="signin-label">Password*</label>
-                    <input
-                        className="signin-input"
-                        type="text"
-                        name="password"
-                        value={password}
-                        onChange={(event) => setPassword(event.target.value)}
-                    />
-                </Form.Field>
-                <Form.Field>
+                    <input className="signin-input" type="text" name="password" value={password} onChange={(event) => setPassword(event.target.value)} required/>
+                </div>
+                <div>
                     <label className="signin-label">Confirm Password*</label>
-                    <input
-                        className="signin-input"
-                        type="text"
-                        name="confirmPassword"
-                        value={confirmPassword}
-                        onChange={(event) => setConfirmPassword(event.target.value)}
-                    />
-                </Form.Field>
-                <Form.Field>
+                    <input className="signin-input" type="text" name="confirmPassword" value={confirmPassword} onChange={(event) => setConfirmPassword(event.target.value)} required/>
+                </div>
+                <div>
                     <label className="signin-label">Company Name</label>
-                    <input
-                        className="signin-input"
-                        type="text"
-                        name="companyName"
-                        value={companyName}
-                        onChange={(event) => setCompanyName(event.target.value)}
-                    />
-                </Form.Field>
-                <Form.Field>
+                    <input className="signin-input" type="text" name="companyName" value={companyName} onChange={(event) => setCompanyName(event.target.value)} />
+                </div>
+                <div>
                     <label className="signin-label">Position</label>
-                    <input
-                        className="signin-input"
-                        type="text"
-                        name="position"
-                        value={position}
-                        onChange={(event) => setPosition(event.target.value)}
-                    />
-                </Form.Field>
-                <Form.Field>
+                    <input className="signin-input" type="text" name="position" value={position} onChange={(event) => setPosition(event.target.value)} />
+                </div>
+                <div>
                     <label className="signin-label">Country</label>
-                    <input
-                        className="signin-input"
-                        type="text"
-                        name="country"
-                        value={country}
-                        onChange={(event) => setCountry(event.target.value)}
-                    />
-                </Form.Field>
-                <Form.Field>
-                    <input
-                        type="checkbox"
-                        id="remember-me"
-                        className="signin-checkbox"
-                        checked={acceptedTerms}
-                        onChange={(event) => setAcceptedTerms(event.target.checked)}
-                    />
+                    <input className="signin-input" type="text" name="country" value={country} onChange={(event) => setCountry(event.target.value)} />
+                </div>
+                <div>
+                    <input type="checkbox" id="acceptedTerms" className="signin-checkbox" checked={acceptedTerms} onChange={handleAcceptedTerms}/>
                     <label className="signin-checkbox-label">By Countinuing you agree to Buggify's <a href="#">Terms and Condition</a> and <a href="#">Privacy Policy</a></label>
-                </Form.Field>
-                <div className="button_ani signin-button">
-                    <Button type="submit" className="button2">Sign In</Button>
+                </div>
+                <div className="button_ani signin-button"  onClick={handleSubmit}>
+                    <button type="submit" className="button2">Sign In</button>
                 </div>
                 <p className="signin-p">
                     Already have an account? <a href="/login">Login Here</a>
                 </p>
-            </Form>
+            </form>
 
 
         </div>
@@ -125,3 +105,4 @@ const SignInForm = () => {
 };
 
 export default SignInForm;
+
--- src/routers/businessUser.js
+++ src/routers/businessUser.js
@@ -1,19 +1,29 @@
+// This file is deprecated
+
 const express = require('express');
 const router = new express.Router();
 const businessUsers = require('../models/userConn');
 
-router.post("/business", async (req, res) => {
-    try {
-        const user = new businessUsers(req.body);
-        const createUser = await user.save();
-        res.status(201).send(createUser);
-    } catch(e){ res.status(400).send('data entry fail'); }
-})
+// router.use(express.json());
+// router.post("/business", async (req, res) => {
+//     try {
+//         console.log(`request come`);
+//         const abc = req.header('Content-type');
+//         console.log(abc);
+//         // const user = new businessUsers(abc);
+//         // const createUser = await user.save();
+//         res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+//         console.log(createUser);
+//         res.status(201).send(createUser);
+//     } catch(e){ res.status(400).send('data entry fail'); }
+// })
 // Getting Data from Database
-router.get("/business/:id", async (req, res) => {
+router.get("/business/:username", async (req, res) => {
     try {
-        const id = req.params.id;
-        const getUser = await businessUsers.find({"_id" : `${id}`});
+        const username = req.params.username;
+        const getUser = await businessUsers.find({"username" : `${username}`});
+        res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+        console.log(getUser);
         res.status(200).send(getUser);
     } catch (e) {
         res.status(400).send(getUser);
@@ -26,6 +36,7 @@ router.patch("/business/:id", async (req, res) => {{
         const updateBusiness = await businessUsers.findByIdAndUpdate(_id, req.body, {
             new: true,
         });
+        res.header("Access-Control-Allow-Origin", "http://localhost:5173");
         res.status(200).send(`email id is changed : ${updateBusiness}`);
     } catch (e) {
         res.status(400).send(`error`)
@@ -39,6 +50,7 @@ router.delete("/business/:id", async (req, res) => {
         if(!req.params.id){
             res.status(400).send(`Bad Request`);
         }
+        res.header("Access-Control-Allow-Origin", "http://localhost:5173");
         res.status(200).send(`removed Business Id : ${deleteBusiness}`);
     } catch (e) {
         res.status(500).send(`Server Connection Problem`);
